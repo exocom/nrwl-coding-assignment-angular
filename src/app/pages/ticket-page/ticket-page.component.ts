b8dae94032fbe1c1617770722df03f9ff19c0c3c
Kalarrs Topham <k-dawg-klark@hotmail.com>
2022-02-26 09:28:01 -0700

    auto save on detail page

--- src/app/pages/ticket-page/ticket-page.component.ts
+++ src/app/pages/ticket-page/ticket-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, pluck, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { filter, map, pluck, shareReplay, switchMap, tap } from 'rxjs/operators';
 import { BackendService } from '../../services/backend.service';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -10,11 +10,11 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './ticket-page.component.html',
   styleUrls: ['./ticket-page.component.scss']
 })
-export class TicketPageComponent implements OnInit {
+export class TicketPageComponent implements OnInit, OnDestroy {
   ticketId = this.route.params.pipe(pluck('ticketId'), shareReplay());
   ticket = this.ticketId.pipe(
     switchMap(ticketId => this.backend.ticket(ticketId)),
-    tap(t => this.formGroup.setValue(t)),
+    tap(t => this.formGroup.reset(t, {onlySelf: true, emitEvent: false})),
   );
   users = this.backend.users();
   ticketWithUsers = combineLatest([this.ticket, this.users]).pipe(
@@ -30,10 +30,24 @@ export class TicketPageComponent implements OnInit {
     completed: [false]
   })
 
+  private subs: Array<Subscription> = [];
+
   constructor(private route: ActivatedRoute, private backend: BackendService, private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    const sub1 = this.formGroup.valueChanges.pipe(
+      filter(() => this.formGroup.valid),
+      switchMap((ticket) => this.backend.update(ticket.id, {...ticket, id: undefined}))
+    ).subscribe((ticket) => {
+      console.log("Saved");
+    });
+
+    this.subs.push(sub1);
+  }
+
+  ngOnDestroy(): void {
+    this.subs?.forEach(s => s?.unsubscribe());
   }
 
 }
