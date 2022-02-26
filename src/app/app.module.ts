d43a811b5b246128eb33d514b883d406b283bb20
Kalarrs Topham <k-dawg-klark@hotmail.com>
2022-02-26 07:29:01 -0700

    move services to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {BackendService} from './backend.service';
+import {BackendService} from './services/backend.service';
 
 @NgModule({
   declarations: [
