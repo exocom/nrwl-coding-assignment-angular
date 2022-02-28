9d8ed8d96ae2a194cb394b4039070951133e8b95
Kalarrs Topham <k-dawg-klark@hotmail.com>
2022-02-28 14:18:59 -0700

    add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { BackendService } from './services/backend.service';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    StoreModule.forRoot({}),
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
